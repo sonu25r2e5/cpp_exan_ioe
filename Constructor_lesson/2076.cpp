56dcac376eef1b785591ac600d7c14550d382338
Sonu kumar <sk7018059@gmail.com>
2025-03-17 12:08:08 +0545

    2075_ashin , 2075 chaitra

--- Constructor_lesson/2076.cpp
+++ Constructor_lesson/2076.cpp
@@ -3,10 +3,28 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
- 
+int volume(int);    // volume for cube
+double volume(double, int); // volume for cylinder 
+long volume(long, int, int);    // volume for rectangle
+
 int main() {
-   
-     
-   
-    return 0;
+    cout << "the volume of cube: " << volume(10) << endl;
+    cout << "the volume of cylinder is: " << volume(2.1,8) << endl; 
+    cout << "The volume of rectangle is : " << volume(100L,75,15);
+    return 0; 
+}
+
+// for cube
+int volume(int s){
+ return (s*s*s);
+}
+
+// for cylinder height and radiu
+
+double volume(double r, int h){
+    return(3.1459*r*r*h);
+}
+
+long volume(long l , int b, int h){
+    return (l *b * h);
 }
